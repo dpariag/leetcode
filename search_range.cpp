0371ec40ef321ad383fda3e2d0c34606bcad9382
dpariag <dpariag@gmail.com>
2016-09-28 21:43:51 -0400

    Skeleton for search_range

---
+++ search_range.cpp
@@ -0,0 +1,22 @@
+// Leetcode: https://leetcode.com/problems/search-for-a-range/ 
+// Given a sorted array of integers, find the starting and ending indices of a given value.
+// Return [-1, -1] if the target is not found.
+// Example: Given [5, 7, 7, 8, 8, 10] and target value 8, return [3, 4].
+
+#include <vector>
+#include <iostream>
+#include <assert.h>
+
+using Numbers = std::vector<int>;
+
+class Solution {
+public:
+    Numbers searchRange(Numbers& nums, int target) {
+        return Numbers();
+    }
+};
+
+int main(int argc, char** argv) {
+    std::cout << argv[0] + 2 << "...OK!" << std::endl;
+    return 0;
+}
