86800ee608f45b704a4d709ef28ff86a7c294431
dpariag <dpariag@gmail.com>
2016-11-30 22:54:00 -0500

    First cut at solution to remove duplicates II

---
+++ remove_duplicates_2.cpp
@@ -0,0 +1,66 @@
+// Leetcode: https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
+// Given a sorted array of integers, enforce a cardinality of at most 2 for each item.
+// Example: Given {1,1,1,2,2,3} the resulting array is {1,1,2,2,3}
+
+#include <vector>
+#include <iostream>
+#include <assert.h>
+#include "numbers.h"
+
+class Solution {
+public:
+    int removeDuplicates(std::vector<int>& nums) {
+        if (nums.size() <= 2) { return nums.size(); }
+        int write = 0, read = 1, item_count = 1;
+
+        while (read < nums.size()) {
+            if (nums[write] == nums[read]) {
+                ++write;
+                nums[write] = nums[read];
+                if (item_count == 0) {
+                    item_count = 1;
+                    ++read;
+                } else {
+                    // item_count == 1
+                    while (read < nums.size() && nums[read] == nums[write]) {++read;}
+                    item_count = 0;
+                }
+            } else {
+                ++write;
+                nums[write] = nums[read];
+                item_count = 1;
+                ++read;
+            }
+        }
+        return write + 1;
+    }
+};
+
+bool test_remove_duplicates(std::vector<int> nums, std::vector<int> expected) {
+    Solution soln;
+    auto num_unique = soln.removeDuplicates(nums);
+
+    for (auto i = 0; i < expected.size(); ++i) {
+        if (expected[i] != nums[i]) { return false; }
+    }
+    return num_unique == expected.size(); 
+}
+
+void test_remove_duplicates() {
+    assert(test_remove_duplicates({}, {}));
+    assert(test_remove_duplicates({1}, {1}));
+    assert(test_remove_duplicates({1,2}, {1,2}));
+    assert(test_remove_duplicates({2,2,2,2,2}, {2,2}));
+    assert(test_remove_duplicates({1,2,14,15}, {1,2,14,15}));
+    assert(test_remove_duplicates({1,1,2}, {1,1,2}));
+    assert(test_remove_duplicates({1,2,2,2,2,3,4,5,5,6}, {1,2,2,3,4,5,5,6}));
+    assert(test_remove_duplicates({1,1,2,2,2,2,3,4,5,5,6}, {1,1,2,2,3,4,5,5,6}));
+    assert(test_remove_duplicates({1,1,1,2,2,2,2,3,4,5,5,6}, {1,1,2,2,3,4,5,5,6}));
+    assert(test_remove_duplicates({1,1,1,2,2,2,2,3,4,5,5,6,6,6,6}, {1,1,2,2,3,4,5,5,6,6}));
+}
+
+int main(int argc, char** argv) {
+    test_remove_duplicates();
+    std::cout << argv[0] + 2 << "...OK!" << std::endl;
+    return 0;
+}
