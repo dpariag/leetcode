f3b0c9d835364156dc57e0b079ea687467a4b902
dpariag <dpariag@gmail.com>
2017-01-03 22:12:55 -0500

    Accepted soluton to reverse words problem

--- reverse_words.cpp
+++ reverse_words.cpp
@@ -2,18 +2,44 @@
 // Given an input string, reverse the string word by word.
 // Example: Given s = "the sky is blue", return "blue is sky the".
 
-// Brute Force:
-// Better:
-// "eulb si yks eht" --> "blue is sky the"
-// "eht yks si eulb" --> "blue is sky the"
+// Brute Force: Allocate a temporary string, copy in words right to left. O(n) time and space.
+// Better: Can be done in place by first reversing the individual words in the string,
+// then reversing the entire string. 
+// Example: "the sky is blue" --> "eht yks si eulb" --> "blue is sky the"
 
 #include <vector>
 #include <iostream>
 #include <assert.h>
 
+// Accepted. 9ms. Beats 25.48% of submissions, ties 52.97% of submissions.
 class Solution {
 public:
+    // Remove leading, trailing, and duplicate inter-word spaces.
+    inline void trim_string(std::string& s) {
+        // trim trailing space
+        while (s.back() == ' ') s.pop_back();
+
+        // skip leading spaces
+        size_t to = 0, from = 0;
+        while (from < s.size() && s[from] == ' ') ++from;
+
+        // shift words left, removing duplicate spaces       
+        while (from < s.size()) {
+            while (from < s.size() && s[from] != ' ') {
+                s[to] = s[from];
+                ++to; ++from;
+            }
+            if (from < s.size() && to < s.size()) s[to++] = ' ';
+            while (from < s.size() && s[from] == ' ') ++from;
+        }
+        // Remove trailing junk
+        s.erase(to);
+    }
+
     void reverseWords(std::string &s) {
+        trim_string(s);
+        
+        // Reverse individual words in the string, in-place
         size_t start = 0;
         while (start < s.size()) {
             while (start < s.size() && s[start] == ' ') ++start;
@@ -27,6 +53,7 @@ public:
             }
             start = end;
         }
+        // Reverse the whole string
         std::reverse(s.begin(), s.end());
     }
 };
@@ -39,15 +66,16 @@ bool test_reverse_words(std::string to_reverse, const std::string& reversed) {
 
 void test_reverse_words() {
     assert(test_reverse_words("", ""));
-    assert(test_reverse_words(" ", " "));
-    assert(test_reverse_words("     ", "     "));
+    assert(test_reverse_words(" ", ""));
+    assert(test_reverse_words("     ", ""));
     assert(test_reverse_words("reverse", "reverse"));
-    assert(test_reverse_words(" reverse", "reverse "));
-    assert(test_reverse_words(" reverse ", " reverse "));
+    assert(test_reverse_words(" reverse", "reverse"));
+    assert(test_reverse_words(" reverse ", "reverse"));
     assert(test_reverse_words("the sky is blue", "blue is sky the"));
     assert(test_reverse_words("the the the", "the the the"));
     assert(test_reverse_words("otto otto", "otto otto"));
-    assert(test_reverse_words("otto     otto", "otto     otto"));
+    assert(test_reverse_words("otto     otto", "otto otto"));
+    assert(test_reverse_words("  otto     otto  ", "otto otto"));
 }
 
 int main(int argc, char** argv) {
