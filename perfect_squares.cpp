7a042567f526127d88c3a2ee9c4582df7587a87b
dpariag <dpariag@gmail.com>
2017-01-18 23:37:14 -0500

    Depth-first-search

--- perfect_squares.cpp
+++ perfect_squares.cpp
@@ -3,107 +3,13 @@
 // Example: Given n = 12, return 3 (12 = 4 + 4 + 4); given n = 13, return 2 (13 = 4 + 9).
 
 // Brute Force: Recursive decomposition of integer, using each 
-// Better:
+// Better: Depth-first-search, pruning any path that is longer than the current minimum.
+// TODO: Breadth-first-search which will be faster at finding a "shortest path"
 
 #include <vector>
 #include <iostream>
 #include <assert.h>
 #include <unordered_map>
-
-using Squares = std::vector<int>;
-
-struct CacheEntry {
-    int sum = 0;
-    int largest_square = 0;
-    
-    CacheEntry(int s, int l) : sum(s), largest_square(l) {}
-    bool operator==(const CacheEntry& other) const {
-        return sum == other.sum && largest_square == other.largest_square;
-    }
-};
-
-struct Hash {
-    size_t operator()(const CacheEntry& c) const {
-        return c.sum * 19 + c.largest_square * 13;
-    }
-};
-
-using Cache = std::unordered_map<CacheEntry, int, Hash>;
-#if 0
-class Solution2 {
-public:
-    int numSquares(int n) {
-        Cache cache;
-        int to_square = m_squares.size() + 1;
-        int square = to_square * to_square;
-        while (square <= n) {
-            m_squares.emplace_back(square);
-            to_square = m_squares.size() + 1;
-            square = to_square * to_square; 
-        }
-        return min_num_squares(cache, n, int(m_squares.size()) - 1);
-    }
-
-    int min_num_squares(Cache& cache, int n, int index) {
-        if (n == 0) { return 0; }
-       
-        auto found = cache.find(CacheEntry(n, m_squares[index]));
-        if (found != cache.end()) {
-            return found->second;
-        }
-
-        int min_ways = std::numeric_limits<int>::max();
-        for (int i = index; i >= 0; --i) {
-            if (m_squares[i] <= n) {
-                int ways = 1 + min_num_squares(cache, n - m_squares[i], i);
-                min_ways = std::min(min_ways, ways);
-            }
-        }
-        cache.emplace(CacheEntry(n, m_squares[index]), min_ways);
-        return min_ways;
-    }
-
-private: 
-    static Squares m_squares;
-};
-Squares Solution2::m_squares = Squares(1,1);
-
-using Table = std::vector<std::vector<int>>;
-class Solution1 {
-public:
-    int numSquares(int n) {
-        if (n == 1) { return 1; }
-
-        Table table;
-        int to_square = table.size() + 1;
-        while ((to_square * to_square) <= n) {
-            table.emplace_back(std::vector<int>(n+1, 0));
-            to_square = table.size() + 1;
-        }
-
-        for (int col = 0; col < table[0].size(); ++col) {
-            table[0][col] = col;
-        }
-
-        int num_squares = n;
-        for (int row = 1; row < table.size(); ++row) {
-            for (int col = 1; col < table[row].size(); ++col) {
-                int square = (row+1) * (row+1);
-                int count = 1;
-                table[row][col] = table[row-1][col];
-                while (col - (count*square) >= 0) {
-                    table[row][col] = std::min(table[row-1][col-(count*square)] + count, 
-                                               table[row][col]);
-                    ++count;
-                }
-            }
-            num_squares = std::min(num_squares, table[row][n]);
-        }
-        std::cout << "n = " << n << " num_squares = " << num_squares << std::endl;
-        return num_squares;
-    }
-};
-#endif
 
 // Accepted. 93ms. Beats 81.08% of solutions and ties < 1% of submissions.
 class Solution {
@@ -123,18 +29,13 @@ public:
     }
     
     int64_t min_num_squares(Cache& cache, int n, int remainder, int num_squares, 
-                        int64_t& cur_min, int to_square) {
+                            int64_t& cur_min, int to_square) {
         if (remainder == 0) {
             return 0;
         } else if (num_squares >= cur_min) {
             return std::numeric_limits<int>::max() - 1;
         }
 
-        //auto found = cache.find(CacheEntry(remainder,to_square));
-        //if (found != cache.end()) {
-        //    return found->second;
-        //}
-      
         int64_t min_ways = std::numeric_limits<int>::max();
         for (int i = to_square; i > 0; --i) {
             int square = i * i;
@@ -146,12 +47,9 @@ public:
                 cur_min = std::min(cur_min, min_ways);
             }
         }
-        //cache.emplace(CacheEntry(remainder, to_square), min_ways);
-        //std::cout << "Cache size = " << cache.size() << std::endl;
         return min_ways;
     }
 };
-
 
 void test_num_squares() {
     Solution soln;
