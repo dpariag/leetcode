5ea76a5723fc9fa4835e8c3f5341bf968950c741
dpariag <dpariag@gmail.com>
2017-01-19 21:39:05 -0500

    Refactoring and cleanup of DFS solution

--- perfect_squares.cpp
+++ perfect_squares.cpp
@@ -2,34 +2,25 @@
 // Given n > 0, find the least number of perfect squares (e.g., 1, 4, 9, 16, ...) which sum to n.
 // Example: Given n = 12, return 3 (12 = 4 + 4 + 4); given n = 13, return 2 (13 = 4 + 9).
 
-// Brute Force: Recursive decomposition of integer, using each 
-// Better: Depth-first-search, pruning any path that is longer than the current minimum.
+// Approach #1: Depth-first-search, pruning any path that is longer than the current minimum.
 // TODO: Breadth-first-search which will be faster at finding a "shortest path"
 
 #include <vector>
 #include <iostream>
 #include <assert.h>
-#include <unordered_map>
+#include <cmath>
 
 // Accepted. 93ms. Beats 81.08% of solutions and ties < 1% of submissions.
 class Solution {
 public:
     int numSquares(int n) {
-        Cache cache;
         int64_t min = n;
-        int to_square = 1;
-        while ((to_square * to_square) <= n) {
-            ++to_square;
-        }
-        --to_square;
-
-        int ret = min_num_squares(cache, n, n, 0, min, to_square);
-        std::cout << "n = " << n << " --> " << ret << std::endl;
-        return ret;
+        int to_square(std::sqrt(n));
+        return dfs_search(n, n, 0, min, to_square);
     }
     
-    int64_t min_num_squares(Cache& cache, int n, int remainder, int num_squares, 
-                            int64_t& cur_min, int to_square) {
+    int64_t dfs_search(int n, int remainder, int num_squares, 
+                       int64_t& cur_min, int to_square) {
         if (remainder == 0) {
             return 0;
         } else if (num_squares >= cur_min) {
@@ -40,7 +31,7 @@ public:
         for (int i = to_square; i > 0; --i) {
             int square = i * i;
             if (remainder - square >= 0) {
-                int64_t min = 1 + min_num_squares(cache, n, remainder-square, num_squares+1, cur_min, i);
+                int64_t min = 1 + dfs_search(n, remainder-square, num_squares+1, cur_min, i);
                 min_ways = std::min(min_ways, min);
             }
             if (remainder == n) {
