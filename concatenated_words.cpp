d2ce633d6611215468c205a69e8369c56723c766
dpariag <dpariag@gmail.com>
2017-06-16 08:54:10 -0400

    Cleanup.

--- concatenated_words.cpp
+++ concatenated_words.cpp
@@ -20,8 +20,7 @@ using Dict = std::unordered_set<std::string>;
 // Accepted. 342ms. Beats 57.34% of submissions and ties 0.5% of submissions.
 class Solution {
 public:
-    bool is_concatenation(const std::string& word, const Dict& dict) {
-        //std::cout << "Word = " << word << std::endl;
+    inline bool is_concatenation(const std::string& word, const Dict& dict) {
         std::vector<bool> is_prefix(word.size(), false);
         std::string prefix;
         prefix.reserve(word.size());
@@ -29,18 +28,12 @@ public:
         for (int i = 0; i < word.size(); ++i) {
             prefix.append(1, word[i]);
             is_prefix[i] = (i < word.size() - 1) && dict.count(prefix) > 0;
-            //std::cout << "word[" << i << "] = " << is_prefix[i] << std::endl;
 
-            if (!is_prefix[i]) {
-                for (int j = 0; j < i; ++j) {
-                    if (is_prefix[j]) {
-                        //std::cout << "found prefix " << word.substr(0, j+1) << std::endl;
-                        //std::cout << "trying " << word.substr(j+1, i-j) << std::endl;
-                        if (dict.count(word.substr(j+1, i-j)) > 0) {
-                            is_prefix[i] = true;
-                            break;
-                        }
-                    }
+            if (is_prefix[i]) { continue; }
+            for (int j = 0; j < i; ++j) {
+                if (is_prefix[j] && dict.count(word.substr(j+1, i-j)) > 0) {
+                    is_prefix[i] = true;
+                    break;
                 }
             }
         }
@@ -52,8 +45,7 @@ public:
         Dict dict(words.begin(), words.end());
         for (const auto& w : words) {
             if (is_concatenation(w, dict)) {
-                concatenations.emplace_back(w);
-                //std::cout << w << " is a concatenation" << std::endl;
+                concatenations.emplace_back(std::move(w));
             }
         }
         return concatenations;
@@ -66,9 +58,6 @@ bool test_concatenated_words(const Words& words, Words expected_concatenations) 
     auto concatenations = soln.findAllConcatenatedWordsInADict(words);
     std::sort(concatenations.begin(), concatenations.end());
     std::sort(expected_concatenations.begin(), expected_concatenations.end());
-    for (int i = 0; i < concatenations.size(); ++i) {
-        //std::cout << "Got: " << concatenations[i] << " expected " << expected_concatenations[i] << std::endl;
-    }
     return concatenations == expected_concatenations;
 }
 
