c887b52be61ab57a99b84413ae60e47f06f43f26
dpariag <dpariag@gmail.com>
2017-09-14 20:39:21 -0400

    Accepted solution to non overlapping intervals.

---
+++ non_overlapping_intervals.cpp
@@ -0,0 +1,62 @@
+// Leetcode: https://leetcode.com/problems/non-overlapping-intervals/description/
+// Given a collection of intervals, find the minimum number of intervals you need to remove to 
+// make the rest of the intervals non-overlapping.
+
+// Brute Force:
+// Better:
+
+#include <vector>
+#include <iostream>
+#include <assert.h>
+
+struct Interval {
+    int start;
+    int end;
+    Interval() : start(0), end(0) {}
+    Interval(int s, int e) : start(s), end(e) {}
+};
+
+using Intervals = std::vector<Interval>;
+
+// Accepted. 9ms. Beats 27.84% of submissions, ties 64.73% of submisssions.
+class Solution {
+public:
+    int eraseOverlapIntervals(Intervals& intervals) {
+        if (intervals.empty()) { return 0; }
+        std::sort(intervals.begin(), intervals.end(),
+                 [](const Interval& a, const Interval& b) { return a.start < b.start; });
+
+        int num_removed = 0;
+        Interval last = intervals[0];
+        for (int i = 1; i < intervals.size(); ++i) {
+            if (intervals[i].start >= last.end) {
+                last = intervals[i];
+            } else {
+                if (intervals[i].end < last.end) {
+                    last = intervals[i];
+                }
+                ++num_removed;
+            }
+        }
+        return num_removed;
+    }
+};
+
+
+bool test_erase_overlapping(Intervals intervals, int expected_removals) {
+    Solution soln;
+    return soln.eraseOverlapIntervals(intervals) == expected_removals;
+}
+
+void test_erase_overlapping() {
+    assert(test_erase_overlapping({{1,2},{2,3}}, 0));
+    assert(test_erase_overlapping({{1,2},{2,3},{3,4},{1,3}}, 1));
+    assert(test_erase_overlapping({{1,2},{1,2},{1,2}}, 2));
+    assert(test_erase_overlapping({{1,2},{1,6},{3,4},{2,4},{2,3}}, 2));
+}
+
+int main(int argc, char** argv) {
+    test_erase_overlapping();
+    std::cout << argv[0] + 2 << "...OK!" << std::endl;
+    return 0;
+}
