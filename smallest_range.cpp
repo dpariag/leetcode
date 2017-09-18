4af3c3d158c2e10bb7e26b3bccaefe9b66948f52
dpariag <dpariag@gmail.com>
2017-09-17 20:47:00 -0400

    Accepted solution to smallest range problem.

--- smallest_range.cpp
+++ smallest_range.cpp
@@ -1,9 +1,10 @@
 // Leetcode:  
 // Given k integer arrays (sorted in ascending order), find the smallest interval [a,b] that includes
-// at least one number from each of the k lists.
+// at least one number from each of the k arrays.
 
-// Brute Force:
-// Better:
+// Brute Force: Start with the range formed by arrays[i][0] for i = 0...k-1, then advance the min
+// min element in the range. O(n*k)
+// Better: Use a binary tree (std::map) to locate the minimum element. O(n*logk)
 
 #include <map>
 #include <vector>
@@ -19,27 +20,33 @@ struct Elem {
     Elem(int a, int i): array(a), index(i) {}
 };
 
+// Accepted. 52ms. Beats 49.56% of submissions, ties 11.26% of submissions.
 class Solution {
 public:
     Array smallestRange(const Arrays& nums) {
-        std::map<int, Elem> range;
+        std::multimap<int, Elem> range; // allow duplicates
+        int smallest_max = std::numeric_limits<int>::max();
 
         for (int i = 0; i < nums.size(); ++i) {
             range.emplace(std::make_pair(nums[i][0], Elem(i, 0)));
+            smallest_max = std::min(smallest_max, nums[i].back());
         }
         
         Array smallest_range({range.begin()->first, (--range.end())->first});
         auto smallest_range_size = smallest_range[1] - smallest_range[0];
         auto cur_range = smallest_range; 
         
-        while (true) {
+        while (range.size() > 1) {
             auto smallest = range.begin()->second;
-            if (smallest.index + 1 == nums[smallest.array].size()) { break; }
             range.erase(range.begin());
-            range.emplace(std::make_pair(nums[smallest.array][smallest.index+1], 
-                                         Elem(smallest.array, smallest.index+1)));
+            if (smallest.index + 1 < nums[smallest.array].size()) {
+                range.emplace(std::make_pair(nums[smallest.array][smallest.index+1], 
+                                             Elem(smallest.array, smallest.index+1)));
+            } 
             cur_range[0] = range.begin()->first;
             cur_range[1] = (--range.end())->first;
+            if (cur_range[0] > smallest_max) { break; }
+
             auto cur_range_size = cur_range[1] - cur_range[0];
             if (cur_range_size < smallest_range_size ||
                 (cur_range_size == smallest_range_size && cur_range[0] < smallest_range[0])) {
@@ -60,10 +67,9 @@ void test_smallest_range() {
     assert(soln.smallestRange({{1,1,1},{2,2}}) == Array({1,2}));
     assert(soln.smallestRange({{1,1,1},{2,2,2}}) == Array({1,2}));
     assert(soln.smallestRange({{1,2,4},{2,4,6}}) == Array({2,2}));
+    assert(soln.smallestRange({{1,2,3},{1,2,3},{1,2,3}}) == Array({1,1}));
     assert(soln.smallestRange({{4,10,15,24,26},{0,9,12,20}, {5,18,22,30}}) == Array({20,24}));
 }
-
-
 
 int main(int argc, char** argv) {
     test_smallest_range();
