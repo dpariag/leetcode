a888b5cd3ec5250501c99feba24b029f56f4ff35
dpariag <dpariag@gmail.com>
2017-09-19 19:35:58 -0400

    Simplifying and refactoring remove invalid parens. Still has bugs

--- remove_invalid_parens.cpp
+++ remove_invalid_parens.cpp
@@ -14,7 +14,7 @@ using Strings = std::vector<std::string>;
 
 class Solution {
 public:
-
+#if 0
     inline int next_index(const std::string& s, const std::vector<int>& indices, int cur_idx) {
         int distance = 0;
         for (int i = cur_idx+1; i < indices.size(); ++i) {
@@ -25,6 +25,7 @@ public:
         }
         return int(indices.size());
     }
+#endif
 
     void generate_balanced(Strings& result, std::string& s, 
                            const std::vector<int>& indices, int idx) {
@@ -36,14 +37,16 @@ public:
 
         int s_idx = indices[idx];
         if (s[s_idx] == '(') {
+            /*
             s.erase(s_idx, 1);
             generate_balanced(result, s, indices, idx+1); 
             s.insert(s_idx, 1, '(');
 
-            int j = s_idx+1;
             // Skip adjacent '('s, since removing them yields duplicates 
+            int j = s_idx+1;
             while (j < s.size() && s[j] == s[s_idx]) { ++j; }
-
+*/
+            int j = s_idx+1;
             // Can remove any (non-adjacent) '(' that lies to the right of s[i], then recurse.
             for (; j < s.size(); ++j) {
                 if (s[j] == '(') {
@@ -54,6 +57,7 @@ public:
             }
         } else if (s[s_idx] == ')') {
             // Remove the unbalanced ')'
+            /*
             s.erase(s_idx, 1);
             generate_balanced(result, s, indices, idx+1);
             s.insert(s_idx, 1, ')');
@@ -61,7 +65,8 @@ public:
             // Skip adjacent ')'s, since removing them yields duplicates
             int j = s_idx-1;
             while (j >= 0 && s[j] == s[s_idx]) { --j; }
-
+            */
+            int j = s_idx-1;
             // Can remove (non-adjacent) ')'s that lie to the left of s[s_idx], then recurse
             for (; j >= 0; --j) {
                 if (s[j] == ')') {
@@ -75,6 +80,7 @@ public:
 
     Strings removeInvalidParentheses(std::string& s) {
         std::vector<int> indices;
+        std::cout << "s = " << s << std::endl;
 
         // Leading ')'s must always be removed.
         while (!s.empty() && s.front() == ')') { s.erase(0, 1); }
@@ -94,39 +100,56 @@ public:
                 }
             }
         }
-#if 0
+#if 1
         std::vector<int> non_consecutive_indices;
         for (int i = 0; i < indices.size(); ++i) {
             if (i == 0) {
                non_consecutive_indices.emplace_back(indices[i]);
             } else if (indices[i] == indices[i-1] + 1 && s[indices[i]] == s[indices[i-1]]) {
+                s.erase(indices[i],1);
                 continue;
             } else {
                non_consecutive_indices.emplace_back(indices[i]);
             }
         }
 #endif
-        for (auto i : indices) {
+        for (auto i : non_consecutive_indices) {
             std::cout << i << " ";
         }
         std::cout << std::endl;
+        std::cout << s << std::endl;
+        std::cout << "*************" << std::endl;
 
         Strings balanced;
-        generate_balanced(balanced, s, indices, 0);
+        generate_balanced(balanced, s, non_consecutive_indices, 0);
+        for (auto& b : balanced) {
+            std::cout << b << " ";
+        }
+        std::cout << std::endl;
         return balanced;
     }
 };
 
-void test_remove_invalid() {
+bool test_remove_invalid(std::string s, Strings expected) {
     Solution soln;
-    //std::string s = "()())()";
-    //std::string s = "()((";
-    std::string s = "((()()";
     auto balanced = soln.removeInvalidParentheses(s);
-    for (auto b : balanced) {
-        std::cout << b << std::endl;
-    }
-    std::cout << std::endl;
+    
+    std::sort(balanced.begin(), balanced.end());
+    std::sort(expected.begin(), expected.end());
+    return balanced == expected;
+}
+
+void test_remove_invalid() {
+    std::string s = "((()()";
+
+    assert(test_remove_invalid("((()()", {"()()", "(())"}));
+    assert(test_remove_invalid("()())()", {"(())()", "()()()"}));
+    assert(test_remove_invalid("()((", {"()"}));
+    assert(test_remove_invalid("(((", {""}));
+    assert(test_remove_invalid("()()(()", {"()()()"}));
+    assert(test_remove_invalid("()((()(())", {"()()(())", "()((()))"}));
+    //assert(test_remove_invalid("(()(()", {"()()"}));
+    //assert(test_remove_invalid("", {"", ""}));
 }
 
 int main(int argc, char** argv) {
