892ccdee5f90be2dab5a04d06de40d9a21d6cf0b
dpariag <dpariag@gmail.com>
2017-10-26 22:03:56 -0400

    Solution to decode string that passes local testing. No memoization.

---
+++ decode_string.cpp
@@ -0,0 +1,61 @@
+// Leetcode: https://leetcode.com/problems/decode-string/description/    
+// Decode a string that has been encoded using the format: k[encoded_string], where the 
+// encoded_string inside the square brackets is repeated exactly k times. Note k > 0.
+
+// Brute Force:
+// Better:
+
+#include <vector>
+#include <cctype>
+#include <iostream>
+#include <assert.h>
+
+class Solution {
+public:
+    std::string decode(const std::string& s, int& offset) {
+        std::string result;
+        int i = offset, repeat = 0;
+        while (i < s.size()) {
+            if (std::isdigit(s[i])) {
+                repeat = (repeat*10) + s[i] - '0';
+            } else if (s[i] == '[') {
+                auto nested = decode(s, ++i);
+                for (; repeat > 0; --repeat) { result.append(nested); }
+            } else if (s[i] == ']') {
+                break;
+            } else {
+                result.append(1, s[i]);
+            }
+            ++i;
+        }
+        offset = i;
+        return result;
+    }
+
+    std::string decodeString(const std::string& s) {
+        int offset = 0;
+        return decode(s, offset);
+    }
+};
+
+void test_decode() {
+    Solution soln;
+    assert(soln.decodeString("") == "");
+    assert(soln.decodeString("a") == "a");
+    assert(soln.decodeString("abcd") == "abcd");
+    assert(soln.decodeString("ab15[c]d") == "abcccccccccccccccd");
+    assert(soln.decodeString("ab2[xy]cd") == "abxyxycd");
+    assert(soln.decodeString("ab2[x99[]y]cd") == "abxyxycd");
+    assert(soln.decodeString("ab2[xy]3[zz]cd") == "abxyxyzzzzzzcd");
+    assert(soln.decodeString("ab2[1[xy]]3[zz]cd") == "abxyxyzzzzzzcd");
+    assert(soln.decodeString("5[r]ab2[1[xy]]3[zz]cd") == "rrrrrabxyxyzzzzzzcd");
+    assert(soln.decodeString("ab2[1[xy]]3[zz]cd2[pt]") == "abxyxyzzzzzzcdptpt");
+    assert(soln.decodeString("ab2[1[x2[O]y]]3[zz]cd2[pt]") == "abxOOyxOOyzzzzzzcdptpt");
+    assert(soln.decodeString("ab2[1[x2[0[N]O]y]]3[zz]cd2[pt]EE") == "abxOOyxOOyzzzzzzcdptptEE");
+}
+
+int main(int argc, char** argv) {
+    test_decode();
+    std::cout << argv[0] + 2 << "...OK!" << std::endl;
+    return 0;
+}
