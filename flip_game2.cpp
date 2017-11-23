34d00a899ddc6e59d50075d5f2815ee000df4691
dpariag <dpariag@gmail.com>
2017-11-23 16:58:36 -0500

    Fix whitespace

--- flip_game2.cpp
+++ flip_game2.cpp
@@ -14,30 +14,30 @@ using Cache = std::unordered_map<std::string, bool>;
 class Solution {
 public:
     bool check_can_win(std::string& s, Cache& cache) {
-      auto found = cache.find(s);
-      if (found != cache.end()) { return found->second; }
+        auto found = cache.find(s);
+        if (found != cache.end()) { return found->second; }
 
-      int i = 0;
-      while (i+1 < int(s.size())) {
-          if (s[i] == '+' && s[i+1] == '+') {
-            // Found a move
-            s[i] = s[i+1] = '-';
-            if (!canWin(s)) {
-              s[i] = s[i+1] = '+';  
-              cache.emplace(s, true);
-              return true;
+        int i = 0;
+        while (i+1 < int(s.size())) {
+            if (s[i] == '+' && s[i+1] == '+') {
+                // Found a move
+                s[i] = s[i+1] = '-';
+                if (!canWin(s)) {
+                    s[i] = s[i+1] = '+';  
+                    cache.emplace(s, true);
+                    return true;
+                }
+                s[i] = s[i+1] = '+';
             }
-            s[i] = s[i+1] = '+';
-          }
-          i += (s[i+1] == '-' ? 2 : 1);
-      }
-      cache.emplace(s, false);
-      return false;
+            i += (s[i+1] == '-' ? 2 : 1);
+        }
+        cache.emplace(s, false);
+        return false;
     }
 
     bool canWin(std::string& s) {
-      Cache cache;
-      return check_can_win(s, cache);  
+        Cache cache;
+        return check_can_win(s, cache);  
     }
 };
 
