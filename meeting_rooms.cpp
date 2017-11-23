f1d11a2920d796fa5fc93ef65e00c4839d4cb42f
dpariag <dpariag@gmail.com>
2017-11-23 17:00:21 -0500

    Skeleton for meeting rooms.

---
+++ meeting_rooms.cpp
@@ -0,0 +1,14 @@
+// Leetcode: 
+//
+
+// Brute Force:
+// Better:
+
+#include <vector>
+#include <iostream>
+#include <assert.h>
+
+int main(int argc, char** argv) {
+    std::cout << argv[0] + 2 << "...OK!" << std::endl;
+    return 0;
+}
