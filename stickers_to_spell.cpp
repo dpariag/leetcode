0b5ab00154f8a37031012c8e050a4026285c52d9
dpariag <dpariag@gmail.com>
2018-09-15 17:12:16 -0700

    Prune DFS search for min stickers to spell problem

--- stickers_to_spell.cpp
+++ stickers_to_spell.cpp
@@ -6,6 +6,7 @@
 
 #include <vector>
 #include <iostream>
+#include <algorithm>
 #include <unordered_map>
 #include <assert.h>
 
@@ -34,21 +35,29 @@ struct Sticker {
   std::vector<int> counts; // count of each letter
 };
 
-// Accepted. 232ms. Beats 23.85% of submissions.
+// Accepted. 148ms. Beats 36.70% of submissions.
 class Solution {
 public:
-  int dfs(std::vector<Sticker>& stickers, Cache& cache, std::string& target) {
-    if (target.empty()) { return 0; }
+  int dfs(std::vector<Sticker>& stickers, Cache& cache, 
+          std::string& target, int len, int& min_len) {
+    if (target.empty()) { 
+      min_len = std::min(len, min_len);
+      return 0; 
+    }
+    if (len >= min_len) { return -1; }
+    
+    auto key = target;
+    std::sort(key.begin(), key.end());
 
     // Check the cache to see if we've searched this target already
-    auto found = cache.find(target);
+    auto found = cache.find(key);
     if (found != cache.end()) { return found->second; }
 
     int min_stickers = -1;
     for (int i = 0; i < stickers.size(); ++i) {
       auto next_target = stickers[i].get_next_target(target);
       if (next_target.size() != target.size()) {
-        auto num_stickers = dfs(stickers, cache, next_target);
+        auto num_stickers = dfs(stickers, cache, next_target, len + 1, min_len);
         if (num_stickers >= 0) {
           min_stickers = (min_stickers == -1) ? 
             1 + num_stickers : 
@@ -56,19 +65,20 @@ public:
         }
       }
     }
-    cache[target] = min_stickers;
+    cache[key] = min_stickers;
     return min_stickers;
   }
 
   int minStickers(std::vector<std::string>& sticker_words, std::string& target) {
     Cache cache;
     std::vector<Sticker> stickers;
+    int min_len = std::numeric_limits<int>::max();
 
     for (const auto& word : sticker_words) {
       stickers.emplace_back(Sticker(word));
     }
-    auto min_stickers = dfs(stickers, cache, target);
-    return min_stickers;
+    auto num_stickers = dfs(stickers, cache, target, 0, min_len);
+    return num_stickers;
   }
 };
 
